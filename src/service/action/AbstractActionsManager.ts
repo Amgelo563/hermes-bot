bf85b7fa70eb02500f9adfb508f1683e0f1a1bbb
Amgelo563 <email@amgelo563.me>
2024-10-19 11:43:48 -0500

    fix: return object id after parsing on actions

--- src/service/action/AbstractActionsManager.ts
+++ src/service/action/AbstractActionsManager.ts
@@ -141,6 +141,8 @@ export abstract class AbstractActionsManager<
     if (Number.isNaN(numberId)) {
       return null;
     }
+
+    return numberId as Data['id'];
   }
 
   protected abstract createBuilderFromCustomId(
